9042814ebf592ffb78a3affb74c0210783edc32d
Danny Jeffery <daniel.p.jeffery@gmail.com>
2015-06-16 16:02:03 +0100

    Teleport to Me

---
+++ TeleportToMe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+using CodeHatch.Engine.Networking;
+using CodeHatch.Common;
+using Oxide.Core;
+using CodeHatch.Networking.Events;
+using CodeHatch.Networking.Events.Entities;
+using CodeHatch.Networking.Events.Entities.Players;
+using CodeHatch.Networking.Events.Players;
+
+namespace Oxide.Plugins
+{
+    [Info("TeleToMe", "Scorpyon", "1.0.1")]
+    public class TeleportToMe : ReignOfKingsPlugin
+    {
+        void Log(string msg) => Puts($"{Title} : {msg}");
+		
+        // USAGE : /teletome "player Name"
+        [ChatCommand("teletome")]
+        private void AdminTeleportToLocation(Player player, string cmd, string[] input)
+        {
+            if(input.Length < 1) return;
+            var playerName = input[0];
+            TeleportPlayerToMe(player, playerName);
+        }
+
+        private void TeleportPlayerToMe(Player player, string playerName)
+        {
+            var target = Server.GetPlayerByName(playerName);
+            if(target == null) return;
+            double posX = target.Entity.Position.x;
+			double posZ = target.Entity.Position.z;
+			
+			posX = player.Entity.Position.x + 2;
+            posZ = player.Entity.Position.z;
+			
+			PrintToChat(player, "Player has been moved to you.");
+        }
+    }
+}
