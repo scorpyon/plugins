864bbdce0a39ddb5b34445784958f978c23e2d72
Danny Jeffery <daniel.p.jeffery@gmail.com>
2015-06-19 10:57:54 +0100

    Created VS Project

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plugins
+{
+    public class Class1
+    {
+    }
+}
