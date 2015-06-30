196bbecb4e86c40e3e5334806ddc2c74d77d7dbb
Danny Jeffery <daniel.p.jeffery@gmail.com>
2015-06-30 16:31:42 +0100

    More bits for VoteRewards

--- VoteReward.cs
+++ VoteReward.cs
@@ -17,30 +17,56 @@ namespace Oxide.Plugins
         int amount = 1000; // this gets rewarded for every vote
         string rewardInterval = "daily"; //This isn't in use yet
 
+        [ChatCommand("getreward")]
+        private void GetMyVoteRewards(Player player, string cmd)
+        {
+            PrintToChat("Getting reward...");
+            Puts("Getting reward...");
+            GiveThisPlayerSomeRewards(player);
+        }
+
 
         void OnPlayerConnected(Player player)
         {
+            //GiveThisPlayerSomeRewards(player);
+        }
+
+        void GiveThisPlayerSomeRewards(Player player)
+        {
             if (player.Name.ToLower() == "server") return;
             WebRequests webRequests = Interface.GetMod().GetLibrary<WebRequests>("WebRequests");
-            
-            var playerId = player.Id;
 
-            timer.Once(30, () => webRequests.EnqueueGet("http://www.dannyjeffery.com/rok-api.php?steamid=" + playerId + "&api=" + rokDotNet_api, (code, response) => WebRequestCallback(code, response, player), this));
+            var playerId = player.Id;
+            var rewardUrl = "http://www.dannyjeffery.com/rokmods/rok-api.php?steamid=" + playerId + "&api=" + rokDotNet_api;
+            //rewardUrl = "http://www.dannyjeffery.com/rok-api.php?steamid=" + playerId + "&api=" + rokDotNet_api;
+            rewardUrl = "http://arqubus.com/rok-api2.php";
+
+            PrintToChat("URL = " + rewardUrl);
+            Puts("URL = " + rewardUrl);
+
+            webRequests.EnqueueGet(rewardUrl, (code, response) => WebRequestCallback(code, response, player), this);
 
         }
+
+
         void WebRequestCallback(int code, string response, Player player)
         {
             var playerName = player.Name;
             var playerId = player.Id;
+            PrintToChat("Code = " + code);
+            Puts("Code = " + code);
 
             if (response == null || code != 200)
             {
                 Puts("Couldn't get an answer from www.dannyjeffery.com for " + playerName);
+                PrintToChat("Couldn't get an answer from www.dannyjeffery.com for " + playerName);
                 return;
             }
+            Puts("Connection successful!");
+            PrintToChat("Connection successful!");
 
             int timesVoted = 0;
-
+            Puts(response);
             Int32.TryParse(response, out timesVoted);
 
             var msgAmnt = timesVoted * amount;
