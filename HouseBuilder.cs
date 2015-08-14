8a393860e957aca870dadba74dc8b43535ee83aa
Danny Jeffery <daniel.p.jeffery@gmail.com>
2015-08-14 16:50:42 +0100

    Housebuilder

---
+++ HouseBuilder.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using CodeHatch.Blocks.Networking.Events;
+using CodeHatch.Blocks.Networking.Events.Local;
+using CodeHatch.Common;
+using CodeHatch.Engine.Networking;
+using CodeHatch.Networking.Events;
+using CodeHatch.Networking.Events.Entities;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("HouseBuilder", "Scorpyon", "1.0.0")]
+    public class HouseBuilder : ReignOfKingsPlugin
+    {
+        // Create a standard 8x10 House
+        [ChatCommand("buildhouse")]
+        private void BuildMyFirstHouse(Player player, string cmd, string[] input)
+        {
+            if (!player.HasPermission("admin"))
+            {
+                PrintToChat(player, "For now, only admins can check locations.");
+                return;
+            }
+            if (input.Length < 3)
+            {
+                PrintToChat(player, "Dont forget to include a width, length and height!");
+                return;   
+            }
+            int width;
+            bool widthOk = Int32.TryParse(input[0], out width);
+            if (!widthOk)
+            {
+                PrintToChat(player, "The width was bad!");
+                return;
+            }
+            int length;
+            bool lengthOk = Int32.TryParse(input[1], out length);
+            if (!lengthOk)
+            {
+                PrintToChat(player, "The length was bad!");
+                return;
+            }
+            int height;
+            bool heightOk = Int32.TryParse(input[2], out height);
+            if (!heightOk)
+            {
+                PrintToChat(player, "The height was bad!");
+                return;
+            }
+            if (width < 4 || length < 4 || height < 3)
+            {
+                PrintToChat(player, "The width and length MUST be at least 4 blocks and the height must be at least 3!");
+                return;
+            }
+
+            BuildARoom(player, width, length, height);
+        }
+
+        private void BuildARoom(Player player, int width, int length, int height)
+        {
+            var startPosition = player.Entity.Position;
+            // material
+            // 0 - (Air)
+            // 1 - Cobblestone
+            // 2 - Stone
+            // 3 - Clay
+            // 4 - Dirt/grass
+            // 5 - Thatch
+            // 6 - Sticks
+            // 7 - Wood
+            // 8 - Logs
+            // 9 - Reinforced Wood
+            byte material = 1; // Cobblestone
+
+            // Floor
+            CreateNewPlatform(player, startPosition, width, length, material);
+            // North Wall
+            var wallStartPositon = new Vector3(startPosition.x, startPosition.y, startPosition.z);
+            CreateLeftWall(player, wallStartPositon, width, length, height, material);
+            ////South Wall
+            CreateRightWall(player, wallStartPositon, width, length, height, material);
+            //// East Wall
+            CreateBackWall(player, wallStartPositon, width, length, height, material);
+            //// West Wall
+            //wallStartPositon = new Vector3(startPosition.x + (width / 2), startPosition.y, startPosition.z - (length / 2) + 1);
+            //CreateLengthWallWithDoor(player, wallStartPositon, width, height, material);
+            // Ceiling
+            var roofStartPositon = new Vector3(startPosition.x, startPosition.y + height + 2, startPosition.z);
+            CreateNewPlatform(player, roofStartPositon, width, length, material);
+        }
+
+        private void CreateBackWall(Player player, Vector3 startPosition, int width, int length, int height, byte material)
+        {
+            Quaternion rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            byte prefabId = 0;
+            Vector3Int playerPos = (Vector3Int)startPosition;
+            var teleEvent = new TeleportEvent(player.Entity, playerPos);
+            EventManager.CallEvent((BaseEvent)teleEvent);
+
+            int setX = playerPos.x - (width / 2);
+            setX = (int)(setX - (double)(setX / 6));
+            int setY = playerPos.y;
+            setY = (int)(setY - (double)(setY / 6));
+            int setZ = playerPos.z - (length / 2);
+            setZ = (int)(setZ - (double)(setZ / 6));
+
+            var originZ = setZ;
+
+            for (var i = 0; i < height; i++)
+            {
+                for (var ii = 0; ii < width; ii++)
+                {
+                    BuildBlockOnThisSquare(setX, setY, setZ, material, rotation, prefabId);
+                    setZ++;
+                }
+                setZ = originZ;
+                setY++;
+            }
+
+            // Tele the player over the platform (to prevent getting stuck
+            EventManager.CallEvent((BaseEvent)new TeleportEvent(player.Entity, new Vector3(startPosition.x, startPosition.y + 1, startPosition.z)));
+        }
+
+        private void CreateLeftWall(Player player, Vector3 startPosition, int width, int length, int height, byte material)
+        {
+            Quaternion rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            byte prefabId = 0;
+            Vector3Int playerPos = (Vector3Int)startPosition;
+            var teleEvent = new TeleportEvent(player.Entity, playerPos);
+            EventManager.CallEvent((BaseEvent)teleEvent);
+
+            int setX = playerPos.x - (width / 2);
+            setX = (int)(setX - (double)(setX / 6));
+            int setY = playerPos.y;
+            setY = (int)(setY - (double)(setY / 6));
+            int setZ = playerPos.z - (length / 2);
+            setZ = (int)(setZ - (double)(setZ / 6));
+
+            BuildWallBlocks(length, height, material, setX, setY, setZ, rotation, prefabId);
+
+            // Tele the player over the platform (to prevent getting stuck
+            EventManager.CallEvent((BaseEvent)new TeleportEvent(player.Entity, new Vector3(startPosition.x, startPosition.y + 1, startPosition.z)));
+        }
+
+        private static void BuildWallBlocks(int length, int height, byte material, int setX, int setY, int setZ,
+            Quaternion rotation, byte prefabId)
+        {
+            var originX = setX;
+
+            for (var i = 0; i < height; i++)
+            {
+                for (var ii = 0; ii < length; ii++)
+                {
+                    BuildBlockOnThisSquare(setX, setY, setZ, material, rotation, prefabId);
+                    setX++;
+                }
+                setX = originX;
+                setY++;
+            }
+        }
+
+        private void CreateRightWall(Player player, Vector3 startPosition, int width, int length, int height, byte material)
+        {
+            Quaternion rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            byte prefabId = 0;
+            Vector3Int playerPos = (Vector3Int)startPosition;
+            var teleEvent = new TeleportEvent(player.Entity, playerPos);
+            EventManager.CallEvent((BaseEvent)teleEvent);
+
+            int setX = playerPos.x - (width / 2);
+            setX = (int)(setX - (double)(setX / 6));
+            int setY = playerPos.y;
+            setY = (int)(setY - (double)(setY / 6));
+            int setZ = playerPos.z + (length / 2) - 1;
+            setZ = (int)(setZ - (double)(setZ / 6));
+
+            BuildWallBlocks(length, height, material, setX, setY, setZ, rotation, prefabId);
+
+            // Tele the player over the platform (to prevent getting stuck
+            EventManager.CallEvent((BaseEvent)new TeleportEvent(player.Entity, new Vector3(startPosition.x, startPosition.y + 1, startPosition.z)));
+
+        }
+
+
+        void CreateNewPlatform(Player player, Vector3 startPosition, int width, int length,byte material)
+        {
+            Quaternion rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            byte prefabId = 0;
+
+            Vector3Int playerPos = (Vector3Int)startPosition;
+            var teleEvent = new TeleportEvent(player.Entity, playerPos);
+            EventManager.CallEvent((BaseEvent)teleEvent);
+
+            int setX = playerPos.x - (width / 2);
+            setX = (int)(setX - (double)(setX / 6));
+            int setY = playerPos.y;
+            setY = (int)(setY - (double)(setY / 6) - 1);
+            int setZ = playerPos.z - (length / 2);
+            setZ = (int)(setZ - (double)(setZ / 6));
+
+            var originX = setX;
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var ii = 0; ii < length; ii++)
+                {
+                    BuildBlockOnThisSquare(setX, setY, setZ, material, rotation, prefabId);
+                    setX++;
+                }
+                setX = originX;
+                setZ++;
+            }
+            // Tele the player over the platform (to prevent getting stuck
+            EventManager.CallEvent((BaseEvent)new TeleportEvent(player.Entity, new Vector3(startPosition.x, startPosition.y + 1, startPosition.z)));
+        }
+
+        private static void BuildBlockOnThisSquare(int setX, int setY, int setZ, byte material, Quaternion rotation, byte prefabId)
+        {
+            var newPosition = new Vector3Int(setX, setY, setZ);
+            var cubeEvent = new CubePlaceEvent(0, newPosition, material, rotation, prefabId, 0.0f);
+            var localCubeEvent = new CubePlaceLocalEvent(cubeEvent, true);
+            EventManager.CallEvent((BaseEvent)cubeEvent);
+            EventManager.CallEvent((BaseEvent)localCubeEvent);
+        }
+    }
+}
--- NightKillOnly.cs
+++ NightKillOnly.cs
@@ -10,157 +10,157 @@ namespace Oxide.Plugins
     [Info("Night Killing Only", "CrZy & Scorpyon", "1.1")]
     public class NightKillOnly : ReignOfKingsPlugin
     {
-        #region Configuration Data
-        bool configChanged;
-		bool broadcastedNightMessage = false;
-        string defaultNightMessage = "It is now Killing Time!";
-        string nightMessage = "";
-        string defaultNightOverMessage = "Killing time is over!";
-        string nightOverMessage = "";
-        private bool _pvPAllowed = true;
+    //    #region Configuration Data
+    //    bool configChanged;
+    //    bool broadcastedNightMessage = false;
+    //    string defaultNightMessage = "It is now Killing Time!";
+    //    string nightMessage = "";
+    //    string defaultNightOverMessage = "Killing time is over!";
+    //    string nightOverMessage = "";
+    //    private bool _pvPAllowed = true;
 
-        #endregion
+    //    #endregion
 
-        void Loaded()
-        {
-            LoadConfigData();
-            timer.Repeat(1, 0, BroadcastNightMessage);
-            LoadPvPCheck();
-        }
+    //    void Loaded()
+    //    {
+    //        LoadConfigData();
+    //        timer.Repeat(1, 0, BroadcastNightMessage);
+    //        LoadPvPCheck();
+    //    }
 
-        protected override void LoadDefaultConfig() => Warning("New configuration file created.");
-        void Warning(string msg) => PrintWarning($"{Title} : {msg}");
+    //    protected override void LoadDefaultConfig() => Warning("New configuration file created.");
+    //    void Warning(string msg) => PrintWarning($"{Title} : {msg}");
 
-        [ChatCommand("allowpvp")]
-        private void LetPlayersPvPAtNight(Player player, string cmd)
-        {
-            if (_pvPAllowed)
-            {
-                _pvPAllowed = false;
-                PrintToChat(player,"You have enabled PvP at Night.");
-            }
-            else
-            {
-                _pvPAllowed = true;
-                PrintToChat(player,"You have disabled PvP at Night.");
-            }
-            SavePvPCheck();
-        }
+    //    [ChatCommand("allowpvp")]
+    //    private void LetPlayersPvPAtNight(Player player, string cmd)
+    //    {
+    //        if (_pvPAllowed)
+    //        {
+    //            _pvPAllowed = false;
+    //            PrintToChat(player,"You have enabled PvP at Night.");
+    //        }
+    //        else
+    //        {
+    //            _pvPAllowed = true;
+    //            PrintToChat(player,"You have disabled PvP at Night.");
+    //        }
+    //        SavePvPCheck();
+    //    }
 
-        private void LoadPvPCheck()
-		{
-            _pvPAllowed = Interface.GetMod().DataFileSystem.ReadObject<bool>("SavedPvPAllowance");
-        }
+    //    private void LoadPvPCheck()
+    //    {
+    //        _pvPAllowed = Interface.GetMod().DataFileSystem.ReadObject<bool>("SavedPvPAllowance");
+    //    }
 
-        private void SavePvPCheck()
-        {
-            Interface.GetMod().DataFileSystem.WriteObject("SavedPvPAllowance", _pvPAllowed);
-        }
+    //    private void SavePvPCheck()
+    //    {
+    //        Interface.GetMod().DataFileSystem.WriteObject("SavedPvPAllowance", _pvPAllowed);
+    //    }
 
-        [ChatCommand("checknight")]
-        private void CheckTimeBlock(Player player, string cmd, string[] args)
-        {
-            string message = "It is currently not night";
+    //    [ChatCommand("checknight")]
+    //    private void CheckTimeBlock(Player player, string cmd, string[] args)
+    //    {
+    //        string message = "It is currently not night";
 
-            if (IsNight())
-                message = "It is currently night";
+    //        if (IsNight())
+    //            message = "It is currently night";
             
-            PrintToChat(player, message);
+    //        PrintToChat(player, message);
 
-        }
+    //    }
 
-        private void OnEntityHealthChange(EntityDamageEvent damageEvent)
-        {
-            GameClock.TimeBlock TimeBlock = GameClock.Instance.CurrentTimeBlock;
+    //    private void OnEntityHealthChange(EntityDamageEvent damageEvent)
+    //    {
+    //        GameClock.TimeBlock TimeBlock = GameClock.Instance.CurrentTimeBlock;
 
-            if (
-                damageEvent.Damage.Amount > 0 // taking damage
-                && damageEvent.Entity.IsPlayer // entity taking damage is player
-                && damageEvent.Damage.DamageSource.IsPlayer // entity delivering damage is a player
-                && damageEvent.Entity != damageEvent.Damage.DamageSource // entity taking damage is not taking damage from self
-                && !IsNight() // Not night time
-                && !PvPAllowed() // Not night time
-            ) 
-            {
-                damageEvent.Cancel("Can Only Kill At Night");
-                damageEvent.Damage.Amount = 0f; 
-                PrintToChat(damageEvent.Damage.DamageSource.Owner, "[FF0000]You can only attack other players at night.[FFFFFF]");
-            }
-        }
+    //        if (
+    //            damageEvent.Damage.Amount > 0 // taking damage
+    //            && damageEvent.Entity.IsPlayer // entity taking damage is player
+    //            && damageEvent.Damage.DamageSource.IsPlayer // entity delivering damage is a player
+    //            && damageEvent.Entity != damageEvent.Damage.DamageSource // entity taking damage is not taking damage from self
+    //            && !IsNight() // Not night time
+    //            && !PvPAllowed() // Not night time
+    //        ) 
+    //        {
+    //            damageEvent.Cancel("Can Only Kill At Night");
+    //            damageEvent.Damage.Amount = 0f; 
+    //            PrintToChat(damageEvent.Damage.DamageSource.Owner, "[FF0000]You can only attack other players at night.[FFFFFF]");
+    //        }
+    //    }
 
-        private bool PvPAllowed()
-        {
-            return _pvPAllowed;
-        }
+    //    private bool PvPAllowed()
+    //    {
+    //        return _pvPAllowed;
+    //    }
 
-        bool IsNight()
-        {
-            GameClock.TimeBlock TimeBlock = GameClock.Instance.CurrentTimeBlock;
+    //    bool IsNight()
+    //    {
+    //        GameClock.TimeBlock TimeBlock = GameClock.Instance.CurrentTimeBlock;
 
-            if (TimeBlock != GameClock.TimeBlock.Dusk && TimeBlock != GameClock.TimeBlock.Night)
-                return false;
+    //        if (TimeBlock != GameClock.TimeBlock.Dusk && TimeBlock != GameClock.TimeBlock.Night)
+    //            return false;
 
-            if (TimeBlock == GameClock.TimeBlock.Dusk && GetCurrentHour() < 21)
-                return false;
+    //        if (TimeBlock == GameClock.TimeBlock.Dusk && GetCurrentHour() < 21)
+    //            return false;
 
-            return true;
-        }
+    //        return true;
+    //    }
 
-        int GetCurrentHour()
-        {
-            string time = GameClock.Instance.TimeOfDayAsClockString(true);
-            string[] timePeices = time.Split(new Char [] {':'});
+    //    int GetCurrentHour()
+    //    {
+    //        string time = GameClock.Instance.TimeOfDayAsClockString(true);
+    //        string[] timePeices = time.Split(new Char [] {':'});
 
-            return Convert.ToInt32(timePeices[0]);
-        }
+    //        return Convert.ToInt32(timePeices[0]);
+    //    }
 
-        void BroadcastNightMessage()
-        {
-            if (!IsNight() && broadcastedNightMessage)
-            {
-                PrintToChat(nightOverMessage);
-                broadcastedNightMessage = false;
-                return;
-            }
+    //    void BroadcastNightMessage()
+    //    {
+    //        if (!IsNight() && broadcastedNightMessage)
+    //        {
+    //            PrintToChat(nightOverMessage);
+    //            broadcastedNightMessage = false;
+    //            return;
+    //        }
 
-            if (IsNight() && !broadcastedNightMessage)
-            {
-                PrintToChat(nightMessage);
-                broadcastedNightMessage = true;
-            }
-        }
+    //        if (IsNight() && !broadcastedNightMessage)
+    //        {
+    //            PrintToChat(nightMessage);
+    //            broadcastedNightMessage = true;
+    //        }
+    //    }
 
-        void LoadConfigData()
-        {
-            nightMessage = GetConfigValue("Messages", "NightStart", defaultNightMessage);
-            nightOverMessage = GetConfigValue("Messages", "NightEnd", defaultNightOverMessage);
+    //    void LoadConfigData()
+    //    {
+    //        nightMessage = GetConfigValue("Messages", "NightStart", defaultNightMessage);
+    //        nightOverMessage = GetConfigValue("Messages", "NightEnd", defaultNightOverMessage);
 
-            if (!configChanged) return;
-            Warning("The configuration file was updated!");
-            SaveConfig();
-        }
+    //        if (!configChanged) return;
+    //        Warning("The configuration file was updated!");
+    //        SaveConfig();
+    //    }
 
-        private T GetConfigValue<T>(string category, string setting, T defaultValue)
-        {
-            var data = Config[category] as Dictionary<string, object>;
-            object value;
+    //    private T GetConfigValue<T>(string category, string setting, T defaultValue)
+    //    {
+    //        var data = Config[category] as Dictionary<string, object>;
+    //        object value;
 
-            if (data == null)
-            {
-                data = new Dictionary<string, object>();
-                Config[category] = data;
-                configChanged = true;
-            }
+    //        if (data == null)
+    //        {
+    //            data = new Dictionary<string, object>();
+    //            Config[category] = data;
+    //            configChanged = true;
+    //        }
 
-            if (data.TryGetValue(setting, out value)) 
-                return (T)Convert.ChangeType(value, typeof(T));
+    //        if (data.TryGetValue(setting, out value)) 
+    //            return (T)Convert.ChangeType(value, typeof(T));
 
-            value = defaultValue;
-            data[setting] = value;
-            configChanged = true;
+    //        value = defaultValue;
+    //        data[setting] = value;
+    //        configChanged = true;
 
-            return (T)Convert.ChangeType(value, typeof(T));
-        }
+    //        return (T)Convert.ChangeType(value, typeof(T));
+    //    }
 
     }
 }
